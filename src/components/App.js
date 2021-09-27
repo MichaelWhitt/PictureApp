37477ee1e3e3c64410ca2bd8132e9fe6b544c14e
MichaelWhitt <michaelwhitt90@gmail.com>
2021-09-27 14:43:39 -0500

    add submit handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-    <div className="ui container" style={{marginTop: "10px"}}>
-        <SearchBar/>
-    </div>)
+class App extends React.Component {
+    onSearchSubmit(t){
+        console.log(t)
+        
+    }
+
+    render(){
+        return (
+        <div className="ui container" style={{marginTop: "10px"}}>
+            <SearchBar onSub={this.onSearchSubmit}/>
+        </div>)
+    }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,15 +3,19 @@ import React from "react";
 class SearchBar extends React.Component{
     state = {term: ""}
 
+    onFormSubmit = (event) =>{
+        event.preventDefault();
+        this.props.onSub(this.state.term)
+    }
 
     render(){
         return(
         <div className="ui segment">
-            <form className="ui form">
+            <form onSubmit={(event) => this.onFormSubmit(event)} className="ui form">
                 <div className="field">
                     <label >Image Search </label>
                     <input type="text" value={this.state.term} onChange={(e)=>this.setState({term:e.target.value})}></input>
-                    {/* <div>{this.state.term}</div> */}
+                    <div>{this.state.term}</div>
                 </div>
             </form>
         </div>
